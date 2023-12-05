ca283101cd46837b6aea454a1ffb16256669baf1
Davidlifelover <142506024+Davidlifelover@users.noreply.github.com>
2023-12-05 22:01:42 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+
+  pagination: {
+      el: '.swiper-pagination',
+  },
+
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+
+  scrollbar: {
+      el: '.swiper-scrollbar',
+  },
+
+  spaceBetween: 16,
+  mousewheel: true,
+  keyboard: true,
+});
+
+const button = document.querySelector('.show-more__text');
+const elements = document.querySelector('.brand__list');
+const icon = document.querySelector('.show-more__img');
+let hidden = true;
+
+let show = () => {
+    console.log(elements);
+    elements.classList.add('visible');
+    hidden = false;
+    button.innerHTML = 'Скрыть';
+    icon.style = 'transform: rotate(180deg)'
+}
+
+let invis = () => {
+    elements.classList.remove('visible');
+    hidden = true;
+    button.innerHTML = 'Читать далее';
+    icon.style = 'transform: rotate(0deg)'
+}
+
+button.addEventListener('click', () => {
+    if (hidden) {
+        show();
+    } else {
+        invis();
+    }
+})
