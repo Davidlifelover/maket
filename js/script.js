0f6071ee810408672f84d6c6728fce92e4c7591a
Davidlifelover <142506024+Davidlifelover@users.noreply.github.com>
2023-12-18 18:33:12 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-    const swiper = new Swiper('.swiper', {
-        direction: 'horizontal',
-        loop: true,
-      
-        pagination: {
-            el: '.swiper-pagination',
-        },
-      
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-      
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
-      
+new Swiper('.swiper', {
+    pagination: {
+        el: '.swiper-pagination',
+
+        clickable: true,
+    }, 
+
+breakpoints: {
+    320: {
+        enabled: true,
+        slidesPerView: 'auto',
         spaceBetween: 16,
-        mousewheel: true,
-        keyboard: true,
-      });
+    },
+    770: {
+        enabled: false,
+        slidesPerView: 'auto',
+        spaceBetween: 0,
+    },
+    
+}
+    });
 
-
+    
 const button = document.querySelector('.show-more__text');
 const elements = document.querySelector('.brand__list');
 const icon = document.querySelector('.show-more__img');
@@ -37,7 +37,7 @@ let show = () => {
 let invis = () => {
     elements.classList.remove('visible');
     hidden = true;
-    button.innerHTML = 'Читать далее';
+    button.innerHTML = 'Показать все';
     icon.style = 'transform: rotate(0deg)'
 }
 
